3718ab26303ec3fdf4ec9dae8479d38dda437fed
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-08-24 19:53:23 +0200

    updated burger menu for mobile screen

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const burger = document.querySelector(".burger-menu");
 const navigation = document.querySelector(".navigation");
-const container = document.querySelector("body");
-
+const back = document.querySelector(".background");
+const features = document.querySelector("li.feat");
+const comp = document.querySelector("li.comp");
+const nav = document.querySelectorAll(".navigation > ul > li ul");
 burger.addEventListener("click", function (e) {
   this.classList.toggle("clicked");
   navigation.classList.toggle("block");
-  container.classList.toggle("op");
+  back.classList.toggle("back");
+});
+features.addEventListener("click", () => {
+  nav[0].classList.toggle("bl");
+  features.classList.toggle("rot");
+});
+comp.addEventListener("click", () => {
+  nav[1].classList.toggle("bl");
+  comp.classList.toggle("rot");
 });
